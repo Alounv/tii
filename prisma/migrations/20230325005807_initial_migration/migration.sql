0b7fe19c8029d03230c0f7e88f995892cd57a2f6
Aloun Vangkeosay <alounv@gmail.com>
2023-03-25 02:00:51 +0100

    Move from sqlite to postgres

--- prisma/migrations/20230325005807_initial_migration/migration.sql
+++ prisma/migrations/20230325005807_initial_migration/migration.sql
@@ -1,9 +1,11 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "name" TEXT
+    "name" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
