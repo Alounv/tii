e9fee4c821582a3e8d8240ea8daa1fbe0fa289a0
Aloun Vangkeosay <alounv@gmail.com>
2023-03-29 16:30:07 +0200

    Clean login page

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -7,14 +7,17 @@ export const Login = component$(() => {
   const loginAction = useAuthSignin();
 
   return (
-    <div>
+    <div class="flex flex-col gap-8 items-center py-6">
       <div>You must login (with a github account) to create an objective</div>
       <Form action={loginAction}>
-        <div class="container">
-          <input type="hidden" name="providerId" value="github" />
-          <div class="login">
-            <button>Login</button>
-          </div>
+        <input type="hidden" name="providerId" value="github" />
+        <div class="login">
+          <button
+            type="submit"
+            class="rounded-lg text-sm font-semibold py-1.5 px-4 bg-gray-600 text-white hover:bg-gray-700"
+          >
+            Login
+          </button>
         </div>
       </Form>
     </div>
